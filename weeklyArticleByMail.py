cf9350c3fb5ea9195eff85b38678c58c5919fc31
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-02-08 19:35:58 +0100

    Init code

---
+++ weeklyArticleByMail.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# Import libs
+import datetime
+import pywikibot
+import smtplib
+import mwparserfromhell
+import requests
+import urllib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from WikiToPlain import Wiki2Plain
+
+# Config libs
+site = pywikibot.Site()
+
+# Fix encoding
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# Fetch article from Wikipedia
+
+yearweek = datetime.date.today().isocalendar()[:2]
+title = 'Wikipedie:Článek týdne/%04d/%02d' % yearweek
+page = pywikibot.Page(site, title)
+code = mwparserfromhell.parse(page.text)
+for template in code.filter_templates():
+	if template.name.strip().lower() == u'čt':
+		for param in template.params:
+			if param.name.strip() == u'název':
+				article = param.value.strip()
+				break
+		break
+
+# Fetch the article's first paragraph
+page = pywikibot.Page(site, article)
+text = str(Wiki2Plain(page.text)).split('\n\n')[0]
+
+# Prepare mails
+
+emailtext = """Článek týdne: %s
+
+%s
+
+Plný text: %s
+
+-- 
+Váš zasílač článků týdne
+
+Kontakt: martin.urbanec@wikimedia.cz
+""" % (article, text, "https://cs.wikipedia.org/wiki/" + article.replace(' ', '_'))
+
+# Send mails!!!
+
+emails = ['martin@urbanec.cz'] # Fetch it from a database
+we = 'urbanecm@tools.wmflabs.org'
+s = smtplib.SMTP('mail.tools.wmflabs.org')
+for email in emails:
+	msg = MIMEText(emailtext.encode('utf-8'))
+	d = datetime.datetime.today()
+	msg['Subject'] = '%s. %s. %s: %s' % (d.day, d.month, d.year, article)
+	msg['From'] = we
+	msg['To'] = email
+	s.sendmail(we, email, msg.as_string())
+s.quit()
